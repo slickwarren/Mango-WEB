5c7f2276962e1d8cc0d48d801bbd086094d74cca
collineps <collineps@gmail.com>
2017-04-10 21:14:44 -0400

    Project Upload
    Adding first test project
    -red capsule test avatar with linked camera
    -camera in 'isometric' view pinned to player
    -plane ground with grass texture

---
+++ Cloud/Assets/Scripts/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * class to control camera movement around the game world
+ * camera currently pegged to player avatar movement
+ * 
+ * CE'18
+ */
+public class CameraController : MonoBehaviour {
+
+	// instance varialbes
+	public GameObject player;
+	private Vector3 offset;
+
+	// functions
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+	}
+}
