6fe76805f9494879b41b8354fec81910dd7bd5d1
Julian Bertini <julianbertini@julians-mbp.wireless.davidson.edu>
2017-04-19 13:42:27 -0400

    make collectibles able to be collected

--- Cloud/Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/AICharacterControl.cs
+++ Cloud/Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/AICharacterControl.cs
@@ -1,8 +1,8 @@
 using System;
 using UnityEngine;
 
-namespace UnityStandardAssets.Characters.ThirdPerson
-{
+namespace UnityStandardAssets.Characters.ThirdPerson {
+	
     [RequireComponent(typeof (UnityEngine.AI.NavMeshAgent))]
     [RequireComponent(typeof (ThirdPersonCharacter))]
     public class AICharacterControl : MonoBehaviour
@@ -40,4 +40,5 @@ namespace UnityStandardAssets.Characters.ThirdPerson
             this.target = target;
         }
     }
-}
+} 
+
