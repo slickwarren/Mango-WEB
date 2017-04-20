22439883e12636a5f5a683df2312ed92afd863fe
collineps <collineps@gmail.com>
2017-04-20 11:27:40 -0400

    Perpendicular Movement and Reset
    created object reset scripts - puts objects back to initial position
    when 'R' key is pressed.
    
    froze rotation about Y-axis using rigidbody. Also freezes movement along
    X-axis using rigidbody. Looking into allowing movement only along axis
    perpendicular to face that character avatar is colliding with.

---
+++ Cloud/Assets/Scripts/PositionReset.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * resets object to initial position on button press
+ * 
+ * CE'18
+ * 4/20/17
+ */
+public class PositionReset : MonoBehaviour {
+
+	// instance variables
+	private Vector3 initialPos;
+
+	// Use this for initialization
+	void Start () {
+		initialPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.R)) {
+			transform.position = initialPos;
+		}
+	}
+}
